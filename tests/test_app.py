05f952225adc80d90f63615c1de4800f0f22e4d1
Valney Filho <valneyf.dev@mail.com>
2024-07-03 11:24:13 -0300

    first commit

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root_deve_retornar_ok_e_hello_world():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Hello World!'}
